32b8720019148a5830d59ffc2d3352f2f223c3c3
Maris <maris.sabirov@gmail.com>
2020-01-11 13:56:43 +0700

    attempt to connect without proxy, if exception ConnectionError try with proxy

--- collect_links.py
+++ collect_links.py
@@ -8,8 +8,6 @@ import matplotlib
 
 from telethon.sync import TelegramClient
 
-# if need proxy unmark
-from telethon import connection
 
 # classes for work with channels
 from telethon.tl.functions.channels import GetParticipantsRequest
@@ -37,15 +35,24 @@ start_channel = config['Telegram']['start_channel']
 for_save = re.split(r'\s',(config['Interest']['for_save']))
 
 
-# if need proxy unmark
+# if need proxy 
 proxy = (proxy_server, proxy_port, proxy_key)
 
-client = TelegramClient(username, api_id, api_hash,
-# if need proxy unmark
-    connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
-    proxy=proxy)
+try:
+    client = TelegramClient(username, api_id, api_hash)
+    client.start()
+except ConnectionError:
+    print('attempt with proxy')
+    from telethon import connection
+    client = TelegramClient(username, api_id, api_hash,
+       connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
+       proxy=proxy)
+    client.start()
+#except Exception as ex:
+#    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+#    message = template.format(type(ex).__name__, ex.args)
+#    print(message)
 
-client.start()
 
 G = nx.DiGraph()
 
