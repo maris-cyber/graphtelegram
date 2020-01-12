9bb54befa805ef07ab201bc272cae661246ff78b
Maris <maris.sabirov@gmail.com>
2020-01-12 18:14:13 +0700

    Test and create folder, create files with channel.to_json info

--- test_tele.py
+++ test_tele.py
@@ -35,6 +35,10 @@ start_channel = config['Telegram']['start_channel']
 
 interest = re.split(r'#',(config['Save']['interest']))
 fpath = config['Save']['folder']
+
+
+
+
         
 if not os.path.isdir(fpath):
     print('Can I create folder: ', fpath,'?')
@@ -53,20 +57,11 @@ if not os.path.isdir(fpath):
 # if need proxy 
 proxy = (proxy_server, proxy_port, proxy_key)
 
-try:
-    client = TelegramClient(username, api_id, api_hash)
-    client.start()
-except ConnectionError:
-    print('attempt with proxy')
-    from telethon import connection
-    client = TelegramClient(username, api_id, api_hash,
-       connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
-       proxy=proxy)
-    client.start()
-#except Exception as ex:
-#    template = "An exception of type {0} occurred. Arguments:\n{1!r}"
-#    message = template.format(type(ex).__name__, ex.args)
-#    print(message)
+from telethon import connection
+client = TelegramClient(username, api_id, api_hash,
+    connection=connection.ConnectionTcpMTProxyRandomizedIntermediate,
+    proxy=proxy)
+client.start()
 
 
 G = nx.DiGraph()
